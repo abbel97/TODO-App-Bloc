ce17414ff1d46e2b3c48f1d4c0f470a2571fd2ee
abell1997 <abeltamerat1997@gmail.com>
2026-05-19 11:57:49 +0100

    Added mock api

---
+++ lib/constants/api_constants.dart
@@ -0,0 +1,8 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String baseUrl =
+      'https://6a0aebf121e445625696e1ce.mockapi.io';
+
+  static const String tasksEndpoint = '$baseUrl/tasks';
+}
